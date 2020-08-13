c940d6c294f4c46d92b3645dd16c9ae213440ef2
JJ Mckeever <mckeeverjohnj@gmail.com>
2020-08-12 21:34:26 -0600

    spoiler alert

---
+++ week2/spoiler.py
@@ -0,0 +1,44 @@
+#! /bin/python3
+
+###
+# To use this code run `pip3 install -r requirements.txt`
+###
+
+import requests
+# https://requests.readthedocs.io/en/master/
+import pprint
+# https://docs.python.org/3/library/pprint.html
+
+# PrettyPrint changes ugly single-line json in to a human readable format
+pp = pprint.PrettyPrinter(indent=2)
+
+# Pull the information about some starships from the API
+response = requests.get('https://swapi.dev/api/starships/')
+# pp.pprint(response)
+
+# Convert to response to JSON
+response = response.json()
+# pp.pprint(response)
+
+# Give us just the list of starships from the response
+starships = response["results"]
+# pp.pprint(starships)
+
+#### PUT ALL NEW CODE BELOW THIS LINE ####
+
+print(len(starships))
+
+# pp.pprint(starships[0])
+
+for starship in starships:
+    print("Name: "+starship["name"])
+    print("   Class: "+starship["starship_class"])
+    # print(type(starship["pilots"]))
+    if len(starship["pilots"]) > 0:
+        print("   Pilots: ")
+    for pilot in starship["pilots"]:
+        # print("Pilot URL: "+pilot)
+        pilot_data = requests.get(pilot)
+        pilot_data_json = pilot_data.json()
+        print("    "+pilot_data_json["name"])
+    # print("Pilots: "+starship["pilots"])
